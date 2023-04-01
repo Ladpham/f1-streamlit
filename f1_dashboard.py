7440ac31d8b2239d4f09b1659099af6b0e3c5893
Ladpham <96309248+Ladpham@users.noreply.github.com>
2023-04-01 00:16:18 -0300

    update commit2

--- f1_dashboard.py
+++ f1_dashboard.py
@@ -1,43 +1,59 @@
 import requests
-import streamlit as st
 import pandas as pd
+import streamlit as st
 import plotly.express as px
 
-def get_f1_data(season, round, endpoint):
-    base_url = 'http://ergast.com/api/f1/'
-    url = f'{base_url}{season}/{round}/{endpoint}.json'
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
+ERGAST_API = "https://ergast.com/api/f1"
+
+def get_race_data(year, round):
+    response = requests.get(f"{ERGAST_API}/{year}/{round}/results.json")
+    data = response.json()["MRData"]["RaceTable"]["Races"]
+    if data:
+        race = data[0]
+        results = race["Results"]
+        df = pd.DataFrame(results)
+        df["year"] = year
+        df["round"] = round
+        df["circuit_name"] = race["Circuit"]["circuitName"]
+        df["position"] = df["position"].astype(int)
+        df["points"] = df["points"].astype(float)
+        df["laps"] = df["laps"].astype(int)
+        df["grid"] = df["grid"].astype(int)
+        return df
     else:
         return None
 
-def race_finishing_positions(data):
-    results = data['MRData']['RaceTable']['Races'][0]['Results']
-    df = pd.DataFrame(results)
-    df['position'] = df['position'].astype(int)
-    fig = px.bar(df, x='Driver', y='position', text='position', title='Race Finishing Positions')
-    fig.update_traces(texttemplate='%{text}s', textposition='outside')
-    return fig
+st.title("F1 Race Analysis")
 
-def qualifying_performance(data):
-    qualifying = data['MRData']['RaceTable']['Races'][0]['QualifyingResults']
-    df = pd.DataFrame(qualifying)
-    df['q1_time'] = pd.to_numeric(df['Q1'].str.extract(r'(\d+\.\d+)')[0])
-    fig = px.line(df, x='Driver', y='q1_time', title='Qualifying Performance (Q1 times)')
-    return fig
+year = st.sidebar.selectbox("Select Year", range(2000, 2022))
+round = st.sidebar.selectbox("Select Round", range(1, 22))
 
-st.title("F1 Dashboard")
+df = get_race_data(year, round)
 
-years = list(range(1950, 2023))
-selected_year = st.sidebar.selectbox("Select Year", years, index=len(years) - 1)
+if df is not None:
+    st.header(f"{df['circuit_name'][0]} - {year}")
 
-rounds = list(range(1, 23))
-selected_round = st.sidebar.selectbox("Select Round", rounds)
+    fig = px.scatter(df, x="position", y="Driver", hover_data=["laps", "Time"], title="Final Positions")
+    st.plotly_chart(fig)
 
-data = get_f1_data(selected_year, selected_round, "qualifying")
-if data:
-    st.plotly_chart(race_finishing_positions(data), use_container_width=True)
-    st.plotly_chart(qualifying_performance(data), use_container_width=True)
+    fig = px.bar(df, x="Driver", y="points", hover_data=["laps", "Time"], title="Driver Rankings")
+    st.plotly_chart(fig)
+
+    # Technical analysis of the fastest lap during the qualifying
+    response = requests.get(f"{ERGAST_API}/{year}/{round}/qualifying.json")
+    data = response.json()["MRData"]["RaceTable"]["Races"]
+    if data:
+        qualifying = data[0]["QualifyingResults"]
+        df_qual = pd.DataFrame(qualifying)
+        df_qual["fastest_lap_rank"] = df_qual["Q1"].rank(method="min").astype(int)
+        df_qual["year"] = year
+        df_qual["round"] = round
+        df_qual["circuit_name"] = data[0]["Circuit"]["circuitName"]
+
+        fig = px.histogram(df_qual, x="Driver", y="fastest_lap_rank", hover_data=["Q1"], title="Fastest Lap Analysis")
+        st.plotly_chart(fig)
+    else:
+        st.write("No qualifying data available")
+
 else:
-    st.error("Error fetching data")
+    st.write("No data available for the selected year and round.")

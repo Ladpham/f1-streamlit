390c0695e0ab6b8adc5c22fb11ab7a2d84d7befa
Ladpham <96309248+Ladpham@users.noreply.github.com>
2023-04-01 00:50:10 -0300

    update

--- f1_dashboard.py
+++ f1_dashboard.py
@@ -26,7 +26,7 @@ def get_race_data(year, round):
 
 st.title("F1 Race Analysis")
 
-year = st.sidebar.selectbox("Select Year", range(2000, 2024))
+year = st.sidebar.selectbox("Select Year", range(2015, 2024))
 round = st.sidebar.selectbox("Select Round", range(1, 22))
 
 df = get_race_data(year, round)
